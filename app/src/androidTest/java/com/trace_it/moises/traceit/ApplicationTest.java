18aef77fc5380b9f567997d1c1bf456814a74d86
kenneth52mm <kenneth52mm@hotmail.com>
2015-05-30 09:25:56 -0600

    Splash screen
    a simple splash screen with our company logo!!

---
+++ app/src/androidTest/java/com/trace_it/moises/traceit/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.trace_it.moises.traceit;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
